1f09d371913f1825983fbf11b52f0a854018657e
abc <abc@gmail.com>
2023-06-06 13:04:54 +0530

    new

---
+++ src/com/cisco/stduent.java
@@ -0,0 +1,17 @@
+package com.cisco;
+
+public class stduent {
+
+	int sid =1;
+	String sname="siva";
+	double smarks=78.50;
+	
+	public static void main(String[] args) {
+
+		stduent s= new stduent();
+		System.out.println(s.sid); 
+		System.out.println(s.sname); 
+		System.out.println(s.smarks);	
+	}
+
+}
